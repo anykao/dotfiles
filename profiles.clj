fed1a93a1f41ed3da8aa6ad79246c2cea2659330
meng <600913@gmail.com>
2013-07-05 12:57:07 +0900

    update profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,6 @@
-{:user {
-        :dependencies  [[lein-ancient  "0.4.2"]
-                        [lein-localrepo  "0.5.0"]
-                        [spyscope  "0.1.3"]]
-        :injections  [(require 'spyscope.core)]
-        }
- }
+{:user  {:plugins  [[lein-ancient  "0.4.2"]
+                    [lein-localrepo  "0.5.0"]]
+         :dependencies  [[spyscope  "0.1.3"]]
+         :injections  [(require 'spyscope.core)]
+         }}
+
