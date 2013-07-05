fb13eb51b66d0b066ca52f13b347cdac6aad8566
meng <600913@gmail.com>
2013-07-05 12:49:01 +0900

    update profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,7 @@
 {:user {
-        :dependencies  [[spyscope  "0.1.3"]]
+        :dependencies  [[lein-ancient  "0.4.2"]
+                        [lein-localrepo  "0.5.0"]
+                        [spyscope  "0.1.3"]]
         :injections  [(require 'spyscope.core)]
         }
  }
